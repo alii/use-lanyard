40a1c123fc6a3a742d948c244a58a315a5300f55
Alistair Smith <hi@alistair.sh>
2023-10-25 20:33:03 +0100

    1.5.1

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,12 @@
 import {defineConfig} from 'tsup';
 
 export default defineConfig({
-	entry: ['./src/index.ts', './src/types.ts'],
+	entry: ['./src/index.ts'],
 	format: ['cjs', 'esm'],
 	dts: true,
 	target: 'node20',
 	sourcemap: true,
 	minify: true,
 	publicDir: 'build',
+	clean: true,
 });
